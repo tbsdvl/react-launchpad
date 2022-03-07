37c39ee2e01a6d669a430258113abbc786c745ea
Triston Burns <tristonburns97@gmail.com>
2022-03-06 19:13:24 -0700

    Add package.json configuration & example for testing a css module with jest

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import styles from "./styles/app.module.css";
 
 const App = () => {
 
@@ -6,7 +7,7 @@ const App = () => {
         <div id="app">
             <section id="welcome">
                 <div className="d-flex flex-column justify-content-center align-items-center p-2 text-light bg-dark">
-                    <h1>Welcome to Launchpad App!</h1>
+                    <h1 className={styles.app}>Welcome to Launchpad App!</h1>
                     <h2>You're ready to launch, happy coding!</h2>
                 </div>
             </section>
